4f0d27b1fc248897df0fcc6af8075d030921e21e
Patel Trupt Raxeshkumar <67566676+trupt-patel@users.noreply.github.com>
2024-12-11 12:23:51 +0530

    Updated tsconfig.json and main.ts for cookie parser.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 import { routeListEnum } from './const/routeListEnum';
