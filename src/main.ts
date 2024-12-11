8333bd3fe532881781a5c70166053b18d0cc9e6b
Patel Trupt Raxeshkumar <67566676+trupt-patel@users.noreply.github.com>
2024-12-11 12:39:21 +0530

    fixed vercel deploy issue.

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
